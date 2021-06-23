2eff31ac8272f25ba2832f8e57e11fcfa4499d2a
Avitrya Singh <avitsingh@paypal.com>
2021-06-24 00:27:06 +0530

    Neater markdown

--- src/components/Rural/BlogDir/Main.js
+++ src/components/Rural/BlogDir/Main.js
@@ -19,50 +19,8 @@ export default function Main(props) {
   const { posts, title } = props;
   console.log(props);
 
-const str = `# Sample blog post
+const str = '# Sample blog post\n#### June 2021 by [404 Found!](/)\nSed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\nAenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.\nSed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.\nCurabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.\nNullam id dolor id nibh ultricies vehicula ut id elit.\nEtiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.\nAenean lacinia bibendum nulla sed consectetur.\n\nVivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.\nDuis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.\nMorbi leo risus, porta ac consectetur ac, vestibulum at eros.\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\nAenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.\nFusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo\nsit amet risus.\n- Praesent commodo cursus magna, vel scelerisque nisl consectetur et.\n Donec id elit non mi porta gravida at eget metus.\n Nulla vitae elit libero, a pharetra augue.\nDonec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.\n1.  Vestibulum id ligula porta felis euismod semper.\n2.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n3.  Maecenas sed diam eget risus varius blandit sit amet non magna.\nCras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.'
 
-             #### 2021 by [404 Found!](/)
-
-             Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
-
-             Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-             Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
-             Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
-
-             Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
-             Nullam id dolor id nibh ultricies vehicula ut id elit.
-
-             Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
-             Aenean lacinia bibendum nulla sed consectetur.
-
-             ## Heading
-
-             Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-             Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-             Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-
-             ### Sub-heading
-
-             Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-             ### Sub-heading
-
-             Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-             Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.
-             Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
-             sit amet risus.
-
-             - Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-             - Donec id elit non mi porta gravida at eget metus.
-             - Nulla vitae elit libero, a pharetra augue.
-
-             Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
-
-             1.  Vestibulum id ligula porta felis euismod semper.
-             2.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-             3.  Maecenas sed diam eget risus varius blandit sit amet non magna.
-
-             Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.`
   return (
 
     <Grid item xs={12} md={8}>
@@ -72,7 +30,7 @@ const str = `# Sample blog post
       <Divider />
         <Markdown options={{ wrapper: 'article' }}>
           {str}
-        </Markdown>;
+        </Markdown>
     </Grid>
   );
 }
