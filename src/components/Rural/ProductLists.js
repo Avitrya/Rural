bec6584f80ca6c0cdb4ca4dc1799957a4c936106
siris <siris@paypal.com>
2021-06-24 14:36:47 +0530

    UI changes in product list

--- src/components/Rural/ProductLists.js
+++ src/components/Rural/ProductLists.js
@@ -19,10 +19,10 @@ class ProductLists extends React.Component {
                             <div className="image">
                                 <img src="../../images/prod1.jpeg" style={{height: '400px'}}/>
                             </div>
-                            <div className="content" style={{textAlign:'justify'}}>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
                                 <a className="header">Terracotta Horse from Panchmura, Bengal </a>
                             </div>
-                            <div className="content">
+                            <div className="content" style={{ backgroundColor:'lightblue'}}>
                                 <a className="header" style={{float:'left'}}> 500/- </a>
                                 <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
                                 <div class="hidden content">Shop</div>
@@ -39,11 +39,11 @@ class ProductLists extends React.Component {
                             <div className="image">
                                 <img src="../../images/prod2.jpeg" style={{height: '400px'}}/>
                             </div>
-                            <div className="content" style={{textAlign:'justify'}}>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
                                 <a className="header">Jaapi from Khargulli, Assam </a>
                             </div>
-                            <div className="content">
-                                <a className="header" style={{float:'left'}}> 500/- </a>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 1000/- </a>
                                 <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
                                 <div class="hidden content">Shop</div>
                                     <div class="visible content">
@@ -59,11 +59,11 @@ class ProductLists extends React.Component {
                             <div className="image">
                                 <img src="../../images/prod3.jpeg" style={{height: '400px'}}/>
                             </div>
-                            <div className="content" style={{textAlign:'justify'}}>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
                                 <a className="header">Little Indian Elephant from Udaipur, Rajasthan</a>
                             </div>
-                            <div className="content">
-                                <a className="header" style={{float:'left'}}> 500/- </a>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 2450/- </a>
                                 <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
                                 <div class="hidden content">Shop</div>
                                     <div class="visible content">
@@ -84,11 +84,11 @@ class ProductLists extends React.Component {
                             <div className="image">
                                 <img src="../../images/prod4.jpeg" style={{height: '400px'}}/>
                             </div>
-                            <div className="content" style={{textAlign:'justify'}}>
+                            <div className="content" style={{textAlign:'justify' , backgroundColor:'lightblue'}}>
                                 <a className="header">Krishna Stone Carving from Solur, Karnataka </a>
                             </div>
-                            <div className="content">
-                                <a className="header" style={{float:'left'}}> 500/- </a>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 2500/- </a>
                                 <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
                                 <div class="hidden content">Shop</div>
                                     <div class="visible content">
@@ -104,11 +104,11 @@ class ProductLists extends React.Component {
                             <div className="image">
                                 <img src="../../images/prod5.jpeg" style={{height: '400px'}}/>
                             </div>
-                            <div className="content" style={{textAlign:'justify'}}>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
                                 <a className="header"> Wooden Kerala Boat from Vengoor, Kerala </a>
                             </div>
-                            <div className="content">
-                                <a className="header" style={{float:'left'}}> 500/- </a>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 250/- </a>
                                 <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
                                 <div class="hidden content">Shop</div>
                                     <div class="visible content">
@@ -124,11 +124,77 @@ class ProductLists extends React.Component {
                             <div className="image">
                                 <img src="../../images/prod6.jpeg" style={{height: '400px'}}/>
                             </div>
-                            <div className="content" style={{textAlign:'justify'}}>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
                                 <a className="header">Coir and Cane Floor mats from Kollam, Kerala</a>
                             </div>
-                            <div className="content">
-                                <a className="header" style={{float:'left'}}> 500/- </a>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 250/- </a>
+                                <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
+                                <div class="hidden content">Shop</div>
+                                    <div class="visible content">
+                                        <i class="shop icon"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <br/>
+            <br/>
+
+            <section className="cards">
+            <div className="ui three column grid">
+                    <div className="column">
+                        <div className="ui fluid card" style={{width: '385px', backgroundColor: 'transparent'}}>
+                            <div className="image">
+                                <img src="../../images/prod7.jpeg" style={{height: '400px'}}/>
+                            </div>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
+                                <a className="header">Kathakali Mask from Kalady, Kerala </a>
+                            </div>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 200/- </a>
+                                <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
+                                <div class="hidden content">Shop</div>
+                                    <div class="visible content">
+                                        <i class="shop icon"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="column">
+                        <div className="ui fluid card" style={{width: '385px', backgroundColor: 'transparent'}}>
+                            <div className="image">
+                                <img src="../../images/prod8.jpeg" style={{height: '400px'}}/>
+                            </div>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
+                                <a className="header"> Sandalwood War Elephant from Mysore, Karnataka </a>
+                            </div>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 10000/- </a>
+                                <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
+                                <div class="hidden content">Shop</div>
+                                    <div class="visible content">
+                                        <i class="shop icon"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="column">
+                        <div className="ui fluid card" style={{width: '385px', backgroundColor: 'transparent'}}>
+                            <div className="image">
+                                <img src="../../images/prod9.jpeg" style={{height: '400px'}}/>
+                            </div>
+                            <div className="content" style={{textAlign:'justify', backgroundColor:'lightblue'}}>
+                                <a className="header">Bamboo and Cane baskets from Birbhum, West Bengal</a>
+                            </div>
+                            <div className="content" style={{backgroundColor:'lightblue'}}>
+                                <a className="header" style={{float:'left'}}> 200/- </a>
                                 <div class="ui vertical animated button" tabindex="0" style={{float:'right'}} >
                                 <div class="hidden content">Shop</div>
                                     <div class="visible content">
