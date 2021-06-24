5f4757b7ff46c2f7d7c1970ea145588f7da1f4d3
Avitrya Singh <avitsingh@paypal.com>
2021-06-24 17:57:57 +0530

    Adding Checkout API

--- src/components/Rural/CheckoutDir/PaymentForm.js
+++ src/components/Rural/CheckoutDir/PaymentForm.js
@@ -4,13 +4,26 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import { PayPalButton } from "react-paypal-button-v2";
+
+let url="";
 
 export default function PaymentForm() {
   return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Pay using PayPal
-      </Typography>
-    </React.Fragment>
+    <PayPalButton
+            amount="0.01"
+            // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+            onSuccess={(details, data) => {
+              alert("Transaction completed by " + details.payer.name.given_name);
+
+              // OPTIONAL: Call your server to save the transaction
+              return fetch("/paypal-transaction-complete", {
+                method: "post",
+                body: JSON.stringify({
+                  orderID: data.orderID
+                })
+              });
+            }}
+          />
   );
 }
